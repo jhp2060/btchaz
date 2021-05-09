914c1f843a51471ed8c991475945ded6fcf3968c
jhp2060 <jhp2060@gmail.com>
2021-05-09 23:35:43 +0900

    feat(entity): Store - added getter, new DBRef field "region", and new field siteURI

--- src/main/java/com/btchaz/api/store/Store.java
+++ src/main/java/com/btchaz/api/store/Store.java
@@ -1,17 +1,23 @@
 package com.btchaz.api.store;
 
+import com.btchaz.api.region.Region;
 import com.btchaz.api.theme.Theme;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collection;
 
+@Getter
 @Document(collection = "stores")
 public class Store {
     @Id
     private String id;
     private String name;
+    private String siteURI;
+    @DBRef
+    private Region region;
     @DBRef
     private Collection<Theme> themes;
 }
