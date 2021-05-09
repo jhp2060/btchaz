b42e52c3e8521ed079acaefc7ad393b690c520ee
jhp2060 <jhp2060@gmail.com>
2021-05-09 23:36:32 +0900

    feat(dto): created BookingDto

---
+++ src/main/java/com/btchaz/api/booking/BookingDto.java
@@ -0,0 +1,32 @@
+package com.btchaz.api.booking;
+
+import com.btchaz.api.store.Store;
+import com.btchaz.api.theme.Theme;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class BookingDto {
+    private String id;
+    private LocalDateTime dateTime;
+    private boolean isOpen;
+    private String themeName;
+    private String themeGenre;
+    private String storeName;
+    private String storeId;
+
+    public BookingDto(Booking b) {
+        this.id = b.getId();
+        this.dateTime = b.getDateTime();
+        this.isOpen = b.isOpen();
+        Theme t = b.getTheme();
+        Store s = t.getStore();
+        this.themeName = t.getName();
+        this.themeGenre =t.getGenre().toString();
+        this.storeName = s.getName();
+        this.storeId = s.getId();
+    }
+}
