87695cf8e0df8b68cb86d5726a733389357020e1
jhp2060 <jhp2060@gmail.com>
2021-05-09 23:35:04 +0900

    feat(entity): theme - added getter and new DBRef field "store"

--- src/main/java/com/btchaz/api/theme/Theme.java
+++ src/main/java/com/btchaz/api/theme/Theme.java
@@ -1,15 +1,18 @@
 package com.btchaz.api.theme;
 
 import com.btchaz.api.booking.Booking;
+import com.btchaz.api.store.Store;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collection;
 
+@Getter
 @Document(collection = "themes")
 public class Theme {
-    enum Genre {
+    public enum Genre {
         Unknown,
         Horror,
         Adult,
@@ -19,6 +22,8 @@ public class Theme {
     private String id;
     private String name;
     private Genre genre;
+    @DBRef
+    private Store store;
     @DBRef
     private Collection<Booking> bookings;
 }
