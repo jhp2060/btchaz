30ec18a19baa0d736f57f32a79bd5d8c47546184
jhp2060 <jhp2060@gmail.com>
2021-05-09 23:33:59 +0900

    feat(entity): booking - added getter and new DBRef field "theme"

--- src/main/java/com/btchaz/api/booking/Booking.java
+++ src/main/java/com/btchaz/api/booking/Booking.java
@@ -1,15 +1,21 @@
 package com.btchaz.api.booking;
 
+import com.btchaz.api.theme.Theme;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Document(collection = "bookings")
 public class Booking {
     @Id
     private String id;
     private LocalDateTime dateTime;
     private boolean isOpen;
+    @DBRef
+    private Theme theme;
 }
