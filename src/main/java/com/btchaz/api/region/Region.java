016fd8235df98cc607b772162b5b1bd20881130e
jhp2060 <jhp2060@gmail.com>
2021-05-09 23:35:55 +0900

    feat(entity): Region - added getter

--- src/main/java/com/btchaz/api/region/Region.java
+++ src/main/java/com/btchaz/api/region/Region.java
@@ -1,13 +1,14 @@
 package com.btchaz.api.region;
 
 import com.btchaz.api.store.Store;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collection;
 
-
+@Getter
 @Document(collection = "regions")
 public class Region {
     @Id
