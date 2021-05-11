fae7bcadcfa93f166096cd609829aa6336623634
jhp2060 <jhp2060@gmail.com>
2021-05-11 22:55:28 +0900

    feat(service): created ThemeService - findById, findByGenre

---
+++ src/main/java/com/btchaz/api/theme/ThemeService.java
@@ -0,0 +1,21 @@
+package com.btchaz.api.theme;
+
+import com.btchaz.api.theme.Theme.Genre;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ThemeService {
+    private ThemeRepository themeRepository;
+
+    public Theme findById(String id) {
+        return themeRepository.findById(id).orElseThrow();
+    }
+    public List<Theme> findByGenre(String g) {
+        Genre genre = Genre.valueOf(g);
+        return themeRepository.findByGenre(genre);
+    }
+}
