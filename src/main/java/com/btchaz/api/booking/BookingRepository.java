c5bdafba02e5c3e9f7db7ffc772d9d13801416ae
jhp2060 <jhp2060@gmail.com>
2022-03-27 23:34:13 +0900

    feat(repo): added "findByThemeAndOpen" to Booking repo

--- src/main/java/com/btchaz/api/booking/BookingRepository.java
+++ src/main/java/com/btchaz/api/booking/BookingRepository.java
@@ -1,5 +1,6 @@
 package com.btchaz.api.booking;
 
+import com.btchaz.api.theme.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.time.LocalDateTime;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Booking findByDateTimeAndOpen (LocalDateTime dt, boolean isOpen);
+    Booking findByThemeAndOpen(Theme theme, boolean isOpen);
 }
